4227f0f42d977cc19aebba04672aebff6ec15cdf
Calvin Montgomery <calzoneman@gmail.com>
2019-04-26 21:33:11 -0700

    Replace fullwidth module with sillytext and add .au

---
+++ sopel/modules/sillytext.py
@@ -0,0 +1,45 @@
+from sopel.module import NOLIMIT, commands, example, rule
+
+# Sorry to anyone whose text editor chokes on this (my terminal hates it)
+upside_down = 'ɐqɔpǝɟbɥıſʞןɯuodbɹsʇnʌʍxʎz'
+
+def australian_char(c):
+    if ord('a') <= ord(c) <= ord('z'):
+        return upside_down[ord(c) - ord('a')]
+    elif c == "'":
+        return ','
+    elif c == '!':
+        return '¡'
+    elif c == '?':
+        return '¿'
+    else:
+        return c
+
+def fullwidth_char(c):
+    if 33 <= ord(c) <= 126:
+        return chr(0xff01 + ord(c) - 33)
+    return c
+
+def convert_fw(text):
+    return ''.join(fullwidth_char(c) for c in text)
+
+def convert_au(text):
+    return ''.join(australian_char(c.lower()) for c in text[::-1])
+
+@commands('fw', 'fullwidth')
+@example('.fw aesthetic')
+def fullwidth(bot, trigger):
+    if trigger.group(2) is None:
+        bot.reply(convert_fw('convert what to fullwidth?'))
+        return NOLIMIT
+
+    bot.say(convert_fw(trigger.group(2)))
+
+@commands('au', 'australia')
+@example(".au g'day mate")
+def australia(bot, trigger):
+    if trigger.group(2) is None:
+        bot.reply(convert_au('convert what to australian?'))
+        return NOLIMIT
+
+    bot.say(convert_au(trigger.group(2)))
